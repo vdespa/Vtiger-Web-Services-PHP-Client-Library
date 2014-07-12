e1181684d07e6679f51a304c6d99c2fa5c29c511
Valentin Despa <info@vdespa.de>
2014-07-12 15:05:16 +0200

    Added a simple method + composer.json definition

---
+++ src/WSClient.php
@@ -0,0 +1,19 @@
+<?php
+namespace Vdespa\Vtiger;
+
+/**
+ * Vtiger Web Services Client
+ *
+ * Class WSClient
+ * @package Vdespa\Vtiger
+ */
+class WSClient
+{
+	/**
+	 * @return string
+	 */
+	public static function sayHelloWorld()
+	{
+		return 'Hello World, Composer!';
+	}
+} 
