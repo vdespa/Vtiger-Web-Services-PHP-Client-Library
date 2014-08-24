3ca05a74e14c00104d005760779f6240b70cc1ee
Valentin Despa <info@vdespa.de>
2014-08-24 18:41:19 +0200

    Updated unit test

--- tests/WSClientTest.php
+++ tests/WSClientTest.php
@@ -4,6 +4,32 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Vdespa\Vtiger;
 
+class WSClientTest extends \PHPUnit_Framework_TestCase
+{
+	protected function setUp()
+	{
+		$this->client = new HttpClient();
+
+		$url = 'http://vtigercrm6x/';
+
+		$config = [
+			'auth' => [
+				'username' => 'system',
+				'accesskey' => 'TkDShdBqAnav2EhG'
+			]
+		];
+
+		$this->wsclient = new Vtiger\WSClient($url, $config);
+	}
+
+	public function testCanCreateClient()
+	{
+		$this->assertInstanceOf('\Vdespa\Vtiger\WSClient', $this->wsclient);
+	}
+}
+
+/*
+
 $url = 'http://vtigercrm6x/';
 
 $config = [
